53b76e37e6fe3d5e66008cbba66c70762b845b8f
Yahddyyp <Yahddyyp@gmail.com>
2026-06-21 14:32:41 +0530

    Inital commit

---
+++ lumina.rb
@@ -0,0 +1,14 @@
+cask "lumina" do
+  version "1.0.0-beta.1"
+  sha256 "..."
+
+  url "https://github.com/Yahddyyp/Lumina/releases/download/v#{version}/Lumina.app.zip"
+  name "Lumina"
+  desc "Another wallpaper engine"
+  homepage "https://github.com/Yahddyyp/Lumina"
+
+  depends_on macos: ">= :sequoia"
+
+  app "Lumina.app"
+end
+
