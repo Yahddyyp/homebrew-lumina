308b640513c6216f4512faf7e99621400dff9166
Yahddyyp <Yahddyyp@gmail.com>
2026-06-21 19:07:18 +0530

    Update SHA for v1.0.1

--- Casks/lumina.rb
+++ Casks/lumina.rb
@@ -1,6 +1,6 @@
 cask "lumina" do
   version "1.0.1"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  sha256 "5fdd3122913b40cce2056769a912d4ab4b9ed4733d194ed461ab62a5e61d2720"
 
   url "https://github.com/Yahddyyp/Lumina/releases/download/v#{version}/Lumina.app.zip"
   name "Lumina"
